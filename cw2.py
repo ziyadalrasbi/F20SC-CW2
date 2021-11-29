5caddcb2d5c280e8cfc607bd9a7753c31e4d0cc3
za45 <za45@hw.ac.uk>
2021-11-29 14:25:09 +0000

    task 2 progress

--- cw2.py
+++ cw2.py
@@ -1,8 +1,13 @@
 import json
+import matplotlib.pyplot as plt
+import pycountry_convert as pc
+import pandas as pd
 
 #create a empty list called data
 data = []
 
+# empty list for getting countries for task 2
+countries = [];
 #open the json file and call it file
 file = open("test.json", 'r')
 Lines = file.readlines()
@@ -22,3 +27,37 @@ file.close()
 for x in data:
     print(x["visitor_country"])
 
+def display_views_by_country(doc_uuid): 
+    for viewer in data:
+        try:
+            if (viewer['env_doc_id'] == doc_uuid):
+                viewer_country = viewer['visitor_country']
+                countries.append(viewer_country)
+        except Exception:
+            pass # do something here for the exception
+    
+    plt.xlabel('country')
+    plt.ylabel('amount')
+    plt.hist(countries)
+    plt.show()
+
+def display_views_by_continent():
+    continents = []
+    for i in countries:
+        try:
+            temp_continent = pc.country_alpha2_to_continent_code(i)
+            continents.append(temp_continent)
+        except Exception:
+            pass # another exception
+    
+    plt.xlabel('continents')
+    plt.ylabel('amount')
+    plt.hist(continents)
+    plt.show()
+
+
+
+
+# testing the functions here
+display_views_by_country('140228202800-6ef39a241f35301a9a42cd0ed21e5fb0')
+display_views_by_continent()
